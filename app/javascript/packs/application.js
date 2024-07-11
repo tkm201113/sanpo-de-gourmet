224c4f478feab6cb4f8d3671d78af8622622f30a
tkm201113 <tkm201113@gmail.com>
2024-07-11 13:24:11 +0900

    a

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './geolocation';
-import './map';
-import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-const application = Application.start();
-const context = require.context("controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
+const application = Application.start()
+const context = require.context("controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 document.addEventListener('DOMContentLoaded', () => {
+  // 必要に応じて初期化コードを追加
 });
