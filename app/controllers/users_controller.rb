49b55204feac55f93462783b29332d0de45f1b35
tkm201113 <tkm201113@gmail.com>
2024-07-14 04:11:08 +0900

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'ユーザーが作成されました'
+      redirect_to home_path, notice: 'ユーザーを作成しました'
     else
+      Rails.logger.debug(@user.errors.full_messages)
       render :new
     end
   end
@@ -15,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   root 'top#index'
 
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  resources :users, only: [:new, :create]
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
