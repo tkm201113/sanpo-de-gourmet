62647515a025f14a9f5c5b8f2cd8467d861b64d2
tkm201113 <tkm201113@gmail.com>
2024-07-22 21:49:20 +0900

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @search_params = {
+      walking_time: params[:walking_time] || 15 # default to 15 minutes if not provided
+    }
   end
 end
